2b72e687aacef72a547fb9f37f56b21bb87921fe
hassanakram <hassan.akram3282@gmail.com>
2015-02-05 14:09:54 -0800

    email Mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,6 +33,7 @@ Rails.application.configure do
   # Generate digests for assets URLs.
   config.assets.digest = true
 
+  config.action_mailer.default_url_options = { :host => "hairdom.herokuapp.com" }
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Specifies the header that your server uses for sending files.
